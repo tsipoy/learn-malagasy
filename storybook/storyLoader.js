da62040a1abc09e06f70050c13e6aa9e637ebffb
student <natacha.har@onja.org>
2021-04-27 03:47:15 +0300

    Adding language switcher button storybook

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -7,6 +7,7 @@ function loadStories() {
   require('../src/components/ActionButton/ActionButton.stories');
   require('../src/components/Example/Example.stories');
   require('../src/components/InputTextarea/PhraseTextarea.stories');
+  require('../src/components/LanguageSwitcherButton/LanguageSwitcherButton.stories');
   require('../src/components/NextButton/NextButton.stories');
   require('../src/components/ToolButton/ToolButton.stories');
   require('./stories/Button/Button.stories');
@@ -17,6 +18,7 @@ const stories = [
   '../src/components/ActionButton/ActionButton.stories',
   '../src/components/Example/Example.stories',
   '../src/components/InputTextarea/PhraseTextarea.stories',
+  '../src/components/LanguageSwitcherButton/LanguageSwitcherButton.stories',
   '../src/components/NextButton/NextButton.stories',
   '../src/components/ToolButton/ToolButton.stories',
   './stories/Button/Button.stories',
